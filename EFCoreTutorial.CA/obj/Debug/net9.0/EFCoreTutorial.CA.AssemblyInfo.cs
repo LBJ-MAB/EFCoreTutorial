146e565569cf1676ffdccced173467bf5e2711ab
LaurieBassett-Jones <laurie.bassett-jones@mab.org.uk>
2025-10-27 10:57:34 +0000

    final commit

--- EFCoreTutorial.CA/obj/Debug/net9.0/EFCoreTutorial.CA.AssemblyInfo.cs
+++ EFCoreTutorial.CA/obj/Debug/net9.0/EFCoreTutorial.CA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFCoreTutorial.CA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3961c5692d0259d5686267f653ade70d2027fea8")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFCoreTutorial.CA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFCoreTutorial.CA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3961c5692d0259d5686267f653ade70d2027fea8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
